a1b8879392386787358097db4f51f9331e2363f3
Jakubpaldyna <jakub.paldyna03@wp.pl>
2024-05-14 15:28:02 +0200

    map added

--- gui_tk.py
+++ gui_tk.py
@@ -1,5 +1,80 @@
 from tkinter import *
 
+import tkintermapview
+
+users=[]
+class User:
+    def __init__(self, imie, nazwisko, posty, miejscowosc):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.posty = posty
+        self.miejscowosc = miejscowosc
+
+def dodaj_użytkownika():
+    imie=entry_imie.get()
+    nazwisko=entry_nazwisko.get()
+    posty=entry_posty.get()
+    miejscowosc=entry_miejscowosc.get()
+    user=User(imie, nazwisko, posty, miejscowosc)
+    users.append(user)
+    print(user.imie)
+    lista_uzytkownikow()
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_posty.delete(0, END)
+    entry_miejscowosc.delete(0, END)
+    entry_imie.focus()
+
+def lista_uzytkownikow():
+    listbox_lista_obietkow.delete(0, END)
+    for idx, user in enumerate(users):
+        listbox_lista_obietkow.insert(idx, f"{user.imie} {user.nazwisko}")
+
+def pokaz_szczeoly_uzytkownika():
+    i=listbox_lista_obietkow.index(ACTIVE)
+    print(i)
+    imie=users[i].imie
+    nazwisko=users[i].nazwisko
+    posty=users[i].posty
+    miejscowosc=users[i].miejscowosc
+
+    label_imie_szczegoly_wartosc.config(text=imie)
+    label_nazwisko_szczegoly_wartosc.config(text=nazwisko)
+    label_posty_szczegoly_wartosc.config(text=posty)
+    label_miejscowosc_szczegoly_wartosc.config(text=miejscowosc)
+
+def usun_uzytkownika():
+    i=listbox_lista_obietkow.index(ACTIVE)
+    users.pop(i)
+    lista_uzytkownikow()
+
+def edytuj_uzytkownika():
+    i=listbox_lista_obietkow.index(ACTIVE)
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_posty.delete(0, END)
+    entry_miejscowosc.delete(0, END)
+
+    entry_imie.insert(0, users[i].imie)
+    entry_nazwisko.insert(0, users[i].nazwisko)
+    entry_posty.insert(0, users[i].posty)
+    entry_miejscowosc.insert(0, users[i].miejscowosc)
+    button_dodaj_uzytkownika.config(text="zapisz zmiany",command=lambda:aktualizuj_uzytkownika(i))
+
+def aktualizuj_uzytkownika(i):
+    users[i].imie=entry_imie.get()
+    users[i].nazwisko=entry_nazwisko.get()
+    users[i].posty=entry_posty.get()
+    users[i].miejscowosc=entry_miejscowosc.get()
+    lista_uzytkownikow()
+
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_posty.delete(0, END)
+    entry_miejscowosc.delete(0, END)
+
+    button_dodaj_uzytkownika.config(text="Dodaj użytkownika", command= dodaj_użytkownika)
+
 
 root=Tk()
 root.geometry('800x700')
@@ -10,16 +85,16 @@ ramka_lista_uzytkownikow=Frame(root)
 ramka_formularz=Frame(root)
 ramka_szczegoly_obiektu=Frame(root)
 
-ramka_lista_uzytkownikow.grid(row=0, column=0)
+ramka_lista_uzytkownikow.grid(row=0, column=0, padx=50)
 ramka_formularz.grid(row=0, column=1)
-ramka_szczegoly_obiektu.grid(row=1, column=0)
+ramka_szczegoly_obiektu.grid(row=1, column=0, columnspan=2, padx=50, pady=50)
 
 # ramka_lista_obietkow
 label_lista_obiektow=Label(ramka_lista_uzytkownikow, text="Lista znajomych:")
 listbox_lista_obietkow=Listbox(ramka_lista_uzytkownikow, width=30)
-button_pokaz_szczegoly=Button(ramka_lista_uzytkownikow, text="Pokaż szczegóły")
-button_usun_uzytkownika=Button(ramka_lista_uzytkownikow, text="usuń")
-button_edytuj_uzytkownika=Button(ramka_lista_uzytkownikow, text="Edytuj")
+button_pokaz_szczegoly=Button(ramka_lista_uzytkownikow, text="Pokaż szczegóły", command=pokaz_szczeoly_uzytkownika)
+button_usun_uzytkownika=Button(ramka_lista_uzytkownikow, text="usuń", command=usun_uzytkownika)
+button_edytuj_uzytkownika=Button(ramka_lista_uzytkownikow, text="Edytuj", command=edytuj_uzytkownika)
 
 label_lista_obiektow.grid(row=0, column=0,)
 listbox_lista_obietkow.grid(row=1, column=0,columnspan=3)
@@ -30,12 +105,52 @@ button_edytuj_uzytkownika.grid(row=2, column=2)
 # ramka_formularz
 
 label_formularz=Label(ramka_formularz, text="Formularz edycji i dodawania:")
+label_imie=Label(ramka_formularz, text="Imię:")
+label_nazwisko=Label(ramka_formularz, text="Nazwisko:")
+label_posty=Label(ramka_formularz, text="Posty:")
+label_miejscowosc=Label(ramka_formularz, text="Miejscowość:")
+entry_imie=Entry(ramka_formularz)
+entry_nazwisko=Entry(ramka_formularz, width=30)
+entry_posty=Entry(ramka_formularz)
+entry_miejscowosc=Entry(ramka_formularz)
+
 label_formularz.grid(row=0, column=0, columnspan=3)
+label_imie.grid(row=1, column=0, sticky=W)
+label_nazwisko.grid(row=2, column=0, sticky=W)
+label_posty.grid(row=3, column=0, sticky=W)
+label_miejscowosc.grid(row=4, column=0, sticky=W)
+entry_imie.grid(row=1, column=1, sticky=W)
+entry_nazwisko.grid(row=2, column=1, sticky=W)
+entry_posty.grid(row=3, column=1, sticky=W)
+entry_miejscowosc.grid(row=4, column=1, sticky=W)
+
+button_dodaj_uzytkownika=Button(ramka_formularz, text="Dodaj użytkownika", command= dodaj_użytkownika)
+button_dodaj_uzytkownika.grid(row=5, column=0, columnspan=2)
 
 
+#ramka szczegoly obietków
+label_opis_obietku=Label(ramka_szczegoly_obiektu, text="Szczegóły obiektu:")
+label_imie_szczegoly=Label(ramka_szczegoly_obiektu, text="Imie:")
+label_imie_szczegoly_wartosc=Label(ramka_szczegoly_obiektu, text='...', width=10)
+label_nazwisko_szczegoly=Label(ramka_szczegoly_obiektu, text="Nazwisko:")
+label_nazwisko_szczegoly_wartosc=Label(ramka_szczegoly_obiektu, text='...', width=10)
+label_posty_szczegoly=Label(ramka_szczegoly_obiektu, text="Posty:")
+label_posty_szczegoly_wartosc=Label(ramka_szczegoly_obiektu, text='...', width=10)
+label_miejscowosc_szczegoly= Label(ramka_szczegoly_obiektu, text="Miejscowość:")
+label_miejscowosc_szczegoly_wartosc=Label(ramka_szczegoly_obiektu, text='...', width=10)
 
+label_opis_obietku.grid(row=0, column=0, sticky=W)
+label_imie_szczegoly.grid(row=1, column=0, )
+label_imie_szczegoly_wartosc.grid(row=1, column=1)
+label_nazwisko_szczegoly.grid(row=1, column=2, )
+label_nazwisko_szczegoly_wartosc.grid(row=1, column=3)
+label_posty_szczegoly.grid(row=1, column=4)
+label_posty_szczegoly_wartosc.grid(row=1, column=5)
+label_miejscowosc_szczegoly.grid(row=1, column=6, )
+label_miejscowosc_szczegoly_wartosc.grid(row=1, column=7)
 
-
+map_widget=tkintermapview.TkinterMapView(ramka_szczegoly_obiektu, width=700, height=300)
+map_widget.grid(row=2, column=0, columnspan=8)
 
 
 
